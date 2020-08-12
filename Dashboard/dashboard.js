82b750f92865ea8c97466aceca532e61ccf3bf48
parthasn <parthasnayak0610@gmail.com>
2020-08-12 14:41:18 +0530

    dashboard first page html

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,3 +1,69 @@
+const restaurant = [
+  {
+    name: "Galaxy",
+    menu: [
+      { item: "Pasta", price: 1000 },
+      { item: "Pizza", price: 1000 },
+    ],
+    image : "https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Garland",
+    menu: [
+      { item: "Garlic", price: 1000 },
+      { item: "Garlen", price: 1000 },
+    ],
+    image : "https://img.freepik.com/free-photo/concept-indian-cuisine-baked-chicken-wings-legs-honey-mustard-sauce-serving-dishes-restaurant-black-plate-indian-spices-wooden-table-background-image_127425-18.jpg?size=626&ext=jpg",
+  },
+  {
+    name: "Momos",
+    menu: [
+      { item: "Momo", price: 1000 },
+      { item: "Mato", price: 1000 },
+    ],
+    image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
+  },
+  {
+    name: "Motnas",
+    menu: [
+      { item: "Motna", price: 1000 },
+      { item: "Motni", price: 1000 },
+    ],
+    image : "https://www.phillymag.com/wp-content/uploads/sites/3/2020/05/alpen-rose-burger.jpg",
+  },
+];
+
+
 window.addEventListener("DOMContentLoaded", () => {
     alert("dashboard");
   });
+
+
+
+  // <div class="card-deck">
+  //     <div class="card">
+  //       <img
+  //         src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+  //         class="card-img-top image-size" alt="...">
+  //       <div class="card-footer">
+  //         <small class="text-muted">Desserts</small>
+  //       </div>
+  //     </div>
+  //     <div class="card">
+  //       <img
+  //         src="https://images.unsplash.com/photo-1574126154517-d1e0d89ef734?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  //         class="card-img-top image-size" alt="...">
+  //       <div class="card-footer">
+  //         <small class="text-muted">Tandoori</small>
+  //       </div>
+  //     </div>
+  //     <div class="card">
+  //       <img
+  //         src=""
+  //         class="card-img-top image-size" alt="...">
+  //       <div class="card-footer">
+  //         <small class="text-muted">Italian</small>
+  //       </div>
+  //     </div>
+
+  //   </div>
