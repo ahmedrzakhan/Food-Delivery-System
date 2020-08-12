da8829452649d7dfc5663e1c9a56270f7e9ec87f
parthasn <parthasnayak0610@gmail.com>
2020-08-12 22:05:31 +0530

    new commit

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,81 +1,81 @@
-const restaurant = [
+let restaurant = [
   {
     name: "Galaxy",
     menu: [
-      { item: "Pasta", price: 1000 },
-      { item: "Pizza", price: 1000 },
+      { item: "Pasta", price: 200 },
+      { item: "Pizza", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
     name: "Garland",
     menu: [
-      { item: "Garlic", price: 1000 },
-      { item: "Garlen", price: 1000 },
+      { item: "Garlic", price: 200 },
+      { item: "Garlen", price: 200 },
     ],
     image : "https://img.freepik.com/free-photo/concept-indian-cuisine-baked-chicken-wings-legs-honey-mustard-sauce-serving-dishes-restaurant-black-plate-indian-spices-wooden-table-background-image_127425-18.jpg?size=626&ext=jpg",
   },
   {
     name: "Momos",
     menu: [
-      { item: "Momo", price: 1000 },
-      { item: "Mato", price: 1000 },
+      { item: "Momo", price: 200 },
+      { item: "Mato", price: 200 },
     ],
     image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
   },
   {
     name: "Moana",
     menu: [
-      { item: "Momo", price: 1000 },
-      { item: "Mato", price: 1000 },
+      { item: "Momo", price: 200 },
+      { item: "Mato", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80",
   },
   {
     name: "Zaika",
     menu: [
-      { item: "Pasta", price: 1000 },
-      { item: "Pizza", price: 1000 },
+      { item: "Pasta", price: 200 },
+      { item: "Pizza", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
     name: "Frank's Chinese Restaurant",
     menu: [
-      { item: "Garlic", price: 1000 },
-      { item: "Garlen", price: 1000 },
+      { item: "Garlic", price: 200 },
+      { item: "Garlen", price: 200 },
     ],
     image : "https://img.freepik.com/free-photo/concept-indian-cuisine-baked-chicken-wings-legs-honey-mustard-sauce-serving-dishes-restaurant-black-plate-indian-spices-wooden-table-background-image_127425-18.jpg?size=626&ext=jpg",
   },
   {
     name: "Pind Baluchi",
     menu: [
-      { item: "Momo", price: 1000 },
-      { item: "Mato", price: 1000 },
+      { item: "Momo", price: 200 },
+      { item: "Mato", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1545247181-516773cae754?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
   },
   {
     name: "Gyani's Restaurant",
     menu: [
-      { item: "Momo", price: 1000 },
-      { item: "Mato", price: 1000 },
+      { item: "Momo", price: 200 },
+      { item: "Mato", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1585937421612-70a008356fbe?ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
   },
   {
     name: "Blue Diamond",
     menu: [
-      { item: "Momo", price: 1000 },
-      { item: "Mato", price: 1000 },
+      { item: "Momo", price: 200 },
+      { item: "Mato", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1574653853027-5382a3d23a15?ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
   },
   {
     name: "Neelam Restaurant",
     menu: [
-      { item: "Momo", price: 1000 },
-      { item: "Mato", price: 1000 },
+      { item: "Momo", price: 200 },
+      { item: "Mato", price: 200 },
     ],
     image : "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
@@ -85,9 +85,20 @@ const restaurant = [
 ];
 
 
+
+
 window.addEventListener("DOMContentLoaded", () => {
   displayRestaurants(restaurant)
-  });
+  localStorage.setItem("restaurants",JSON.stringify(restaurant))
+
+  let admin = document.getElementById("admin")
+  admin.addEventListener("click", toAdminPage)
+})
+
+
+const toAdminPage = () =>{
+  window.location.href = "Admin.html"
+}
 
 
   
