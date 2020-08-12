0da7c1b09460bd2e8873e9433eb5b472c2fa158f
parthasn <parthasnayak0610@gmail.com>
2020-08-12 17:06:19 +0530

    javascript file for dashboard updated

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -24,7 +24,7 @@ const restaurant = [
     image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
   },
   {
-    name: "Momos",
+    name: "Moana",
     menu: [
       { item: "Momo", price: 1000 },
       { item: "Mato", price: 1000 },
@@ -32,7 +32,7 @@ const restaurant = [
     image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
   },
   {
-    name: "Galaxy",
+    name: "Zaika",
     menu: [
       { item: "Pasta", price: 1000 },
       { item: "Pizza", price: 1000 },
@@ -40,7 +40,7 @@ const restaurant = [
     image : "https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
-    name: "Garland",
+    name: "Frank's Chinese Restaurant",
     menu: [
       { item: "Garlic", price: 1000 },
       { item: "Garlen", price: 1000 },
@@ -48,7 +48,7 @@ const restaurant = [
     image : "https://img.freepik.com/free-photo/concept-indian-cuisine-baked-chicken-wings-legs-honey-mustard-sauce-serving-dishes-restaurant-black-plate-indian-spices-wooden-table-background-image_127425-18.jpg?size=626&ext=jpg",
   },
   {
-    name: "Momos",
+    name: "Pind Baluchi",
     menu: [
       { item: "Momo", price: 1000 },
       { item: "Mato", price: 1000 },
@@ -56,13 +56,30 @@ const restaurant = [
     image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
   },
   {
-    name: "Momos",
+    name: "Gyani's Restaurant",
     menu: [
       { item: "Momo", price: 1000 },
       { item: "Mato", price: 1000 },
     ],
     image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
   },
+  {
+    name: "Blue Diamond",
+    menu: [
+      { item: "Momo", price: 1000 },
+      { item: "Mato", price: 1000 },
+    ],
+    image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
+  },
+  {
+    name: "Neelam Restaurant",
+    menu: [
+      { item: "Momo", price: 1000 },
+      { item: "Mato", price: 1000 },
+    ],
+    image : "https://curlytales.com/wp-content/uploads/2019/08/north-indian-food-2-1-735x413.jpg",
+  },
+  
  
 
 ];
@@ -73,45 +90,41 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
 
+  
+
+
   function displayRestaurants(array){
     let cont = document.getElementById("restaurants")
+    // creating row
+    let row = document.createElement("div")
+     row.setAttribute("class", "row")
+    for(let i = 0; i < array.length;i++){
+      //creating column for restaurants
+      let col = document.createElement("div")
+      col.setAttribute("class", "col-3 mb-2")
+      // creating card for each restaurant
+      let card = document.createElement("div")
+      card.setAttribute("class","card")
+      // image of each restaurant
+      let image = document.createElement("img")
+      image.src = array[i].image
+      image.alt = array[i].name
+      image.setAttribute("class", "card-image-top image-size")
+      image.style.borderRadius = "5px"
+      // for name of restaurant
+      let cardFooter = document.createElement("div")
+      cardFooter.setAttribute("class", "card-footer p-2")
+      cardFooter.textContent = array[i].name
+      
+      card.append(image,cardFooter)
 
-    for(let i = 0; i < array.length; i += 4){
+      col.append(card)
 
-      let cardDeck = document.createElement("div")
-      cardDeck.setAttribute("class", "card-deck")
-
-      for(let j = 0; j < 4; j++){
-
-        let card = document.createElement("div")
-        card.setAttribute("class", "card")
-        card.style.borderRadius = "5px"
-  
-        let image = document.createElement("img")
-        image.src = array[j].image
-        image.alt = array[j].name
-        image.setAttribute("class", "card-image-top")
-        image.setAttribute("class", "image-size")
-        image.style.borderRadius = "5px"
-  
-        let cardFooter = document.createElement("div")
-        cardFooter.setAttribute("class", "card-footer")
-  
-        let name = document.createElement("p")
-        name.setAttribute("class","text-muted")
-        name.textContent = array[j].name
-  
-        cardFooter.append(name)
-  
-        card.append(image,cardFooter)
-
-        cardDeck.append(card)
-      }
-      cont.append(cardDeck)
-     
-   }
-   
+      row.append(col)
+    }
+    cont.append(row)
   }
 
+  
 
 
